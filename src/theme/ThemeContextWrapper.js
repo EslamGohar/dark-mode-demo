3ac82b3aaaf3ae5322a060a80131de969e0ff97d
Eslam Gohar <islamgoher1@gmail.com>
2023-08-07 18:26:23 +0300

    Replacing Switch cases with Map object in ThemeContextWrapper component

--- src/theme/ThemeContextWrapper.js
+++ src/theme/ThemeContextWrapper.js
@@ -1,30 +1,43 @@
-import { useState, useEffect } from 'react'
-import { ThemeContext, themes } from '../context/ThemeContext'
+import { useState, useEffect } from "react";
+import { ThemeContext, themes } from "../context/ThemeContext";
 
 export default function ThemeContextWrapper({ children }) {
-	const [theme, setTheme] = useState(themes.dark)
+  const [theme, setTheme] = useState(themes.dark);
+
+  function changeTheme(theme) {
+    setTheme(theme);
+  }
+
+  useEffect(() => {
+    //  switch (theme) {
+    //    case themes.light:
+    //      document.body.classList.add("white-content")
+    //      break
+    //    case themes.dark:
+    //    default:
+    //      document.body.classList.remove("white-content")
+    //      break
+    //  }
+
+    // Using Map object as alternative to Switch cases.
+    const themeClassMap = new Map([
+      [themes.light, "white-content"],
+      [themes.dark, ""],
+    ]);
+
+    const themeClassToAdd = themeClassMap.get(theme);
+    document.body.classList.toggle(
+      "white-content",
+      themeClassToAdd === "white-content"
+    );
+  }, [theme]);
+
 	
-	function changeTheme(theme) { 
-		setTheme(theme)
-	}
-	
-	useEffect(() => { 
-		switch (theme) { 
-			case themes.light:
-				document.body.classList.add('white-content')
-				break;
-			case themes.dark:
-			default:
-				document.body.classList.remove('white-content')
-				break;
-		}
-	}, [theme])
-	
-	return (
-	  <div>
-		  <ThemeContext.Provider value={{ theme, changeTheme }}>
-			  { children }
-		  </ThemeContext.Provider>
-	 	</div>
-  	)
+  return (
+    <div>
+      <ThemeContext.Provider value={{ theme, changeTheme }}>
+        {children}
+      </ThemeContext.Provider>
+    </div>
+  );
 }
